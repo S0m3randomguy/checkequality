fd240df779f5c3eeb2ed0b79549b72c0e0d71b5e
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2024-05-11 12:46:04 +0200

    Separated array equality check into new method

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
- 
+
+namespace checkEquality {
+
+    function arrayEqual(first: any[], second: any[]): boolean {
+        if (first.length !== second.length) return false;
+        return first.every((value, index) => equal(value, second[index]));
+    }
+
+    export function equal(first: any, second: any): boolean {
+        if (Array.isArray(first) && Array.isArray(second)) {
+            return arrayEqual(first as any[], second as any[]);
+        }
+        return first === second;
+    }
+}
