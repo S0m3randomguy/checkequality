cf52410fc1ed6110a280e70f02aeab242003a862
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2024-05-11 12:53:15 +0200

    Added objectEqual method

--- main.ts
+++ main.ts
@@ -6,9 +6,23 @@ namespace checkEquality {
         return first.every((value, index) => equal(value, second[index]));
     }
 
+    function objectEqual(first: {[key: string]: any}, second: {[key: string]: any}): boolean {
+        let firstKeys = Object.keys(first);
+        let secondKeys = Object.keys(second);
+        if (firstKeys.length !== secondKeys.length) return false;
+        
+        firstKeys.sort();
+        secondKeys.sort();
+        if (!arrayEqual(firstKeys, secondKeys)) return false;
+        return firstKeys.every((value) => equal(first[value], second[value]));
+    }
+
     export function equal(first: any, second: any): boolean {
         if (Array.isArray(first) && Array.isArray(second)) {
             return arrayEqual(first as any[], second as any[]);
+        }
+        if (typeof first === "object" && typeof second === "object") {
+            return objectEqual(first as object, second as object);
         }
         return first === second;
     }
